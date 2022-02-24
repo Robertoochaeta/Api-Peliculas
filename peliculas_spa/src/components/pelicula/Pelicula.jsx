271aced693fd7b6a21c51aa349b25bc97f6d5e8b
Roberto <mario24roberto@gmail.com>
2022-02-24 12:14:01 -0600

    Agregando el componente de peliculas

---
+++ peliculas_spa/src/components/pelicula/Pelicula.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Peliculas=()=>{
+    return(
+        <>
+        <div>
+
+            <h2>Peliculas</h2>
+        </div>
+
+        <div>
+            <p>Peliculas</p>
+        </div>
+        <table>
+            <ul>
+                <li>La mascara</li>
+                <li>La Masacre de texas</li>
+            </ul>
+        </table>
+        </>
+        
+    );
+}
+export default Peliculas;
---
+++ peliculas_spa/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Peliculas from './components/pelicula/Pelicula'
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Peliculas></Peliculas>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
